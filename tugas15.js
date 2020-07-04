72ebf9b83146e4467198391323f82da104980acd
hugoiman <hugogiman17@gmail.com>
2020-07-04 13:40:04 +0700

    Tugas 15 Javascript

---
+++ tugas15.js
@@ -0,0 +1,8 @@
+var AB = 8;
+var BC = 6;
+
+var AC = Math.sqrt(Math.pow(AB, 2) + Math.pow(BC, 2));
+
+console.log("Nilai AB: ", AB);
+console.log("Nilai BC: ", BC);
+console.log("Nilai AC (siku-siku): ", AC);
